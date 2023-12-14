7cc87f9ca2654c4bd16a4f9a5ee11b73610c74b2
Chang Eden <115127072+changeden@users.noreply.github.com>
2023-12-14 17:57:12 +0800

    linked lists
    Leet code - pre review -  linked lists

---
+++ add2.py
@@ -0,0 +1,134 @@
+"""
+File: add2.py
+Name:
+------------------------
+TODO:
+"""
+
+import sys
+
+
+class ListNode:
+    def __init__(self, data=0, pointer=None):
+        self.val = data
+        self.next = pointer
+
+
+def add_2_numbers(l1: ListNode, l2: ListNode) -> ListNode:
+    #######################
+    #                     #
+    #        TODO:        #
+    #                     #
+    #######################
+    cur1 = l1
+    cur2 = l2
+    l1_number = ''
+    l2_number = ''
+    new_l1_num = ''
+    new_l2_num = ''
+    while cur1 is not None:
+        num1 = cur1.val
+        l1_number += str(num1)
+        cur1 = cur1.next
+    while cur2 is not None:
+        num2 = cur2.val
+        l2_number += str(num2)
+        cur2 = cur2.next
+    for i in range(len(l1_number)-1,-1,-1):
+        num1 = str(l1_number[i])
+        new_l1_num += num1
+    for i in range(len(l2_number)-1,-1,-1):
+        num2 = str(l2_number[i])
+        new_l2_num += num2
+    new_num = str(int(new_l1_num)+int(new_l2_num))
+
+    list_node = None
+    node = None
+    for i in range(len(new_num)-1,-1,-1):
+        number = new_num[i]
+        if node is None:
+        # if list_node is None:
+            node = ListNode(int(number),None)
+            list_node = node
+        else:
+            node.next = ListNode(int(number),None)
+            node = node.next
+    return list_node
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+
+def traversal(head):
+    """
+    :param head: ListNode, the first node to a linked list
+    -------------------------------------------
+    This function prints out the linked list starting with head
+    """
+    cur = head
+    while cur.next is not None:
+        print(cur.val, end='->')
+        cur = cur.next
+    print(cur.val)
+
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print('Error: Please type"python3 add2.py test1"')
+    else:
+        if args[0] == 'test1':
+            l1 = ListNode(2, None)
+            l1.next = ListNode(4, None)
+            l1.next.next = ListNode(3, None)
+            l2 = ListNode(5, None)
+            l2.next = ListNode(6, None)
+            l2.next.next = ListNode(4, None)
+            ans = add_2_numbers(l1, l2)
+            print('---------test1---------')
+            print('l1: ', end='')
+            traversal(l1)
+            print('l2: ', end='')
+            traversal(l2)
+            print('ans: ', end='')
+            traversal(ans)
+            print('-----------------------')
+        elif args[0] == 'test2':
+            l1 = ListNode(9, None)
+            l1.next = ListNode(9, None)
+            l1.next.next = ListNode(9, None)
+            l1.next.next.next = ListNode(9, None)
+            l1.next.next.next.next = ListNode(9, None)
+            l1.next.next.next.next.next = ListNode(9, None)
+            l1.next.next.next.next.next.next = ListNode(9, None)
+            l2 = ListNode(9, None)
+            l2.next = ListNode(9, None)
+            l2.next.next = ListNode(9, None)
+            l2.next.next.next = ListNode(9, None)
+            ans = add_2_numbers(l1, l2)
+            print('---------test2---------')
+            print('l1: ', end='')
+            traversal(l1)
+            print('l2: ', end='')
+            traversal(l2)
+            print('ans: ', end='')
+            traversal(ans)
+            print('-----------------------')
+        elif args[0] == 'test3':
+            l1 = ListNode(0, None)
+            l2 = ListNode(0, None)
+            ans = add_2_numbers(l1, l2)
+            print('---------test3---------')
+            print('l1: ', end='')
+            traversal(l1)
+            print('l2: ', end='')
+            traversal(l2)
+            print('ans: ', end='')
+            traversal(ans)
+            print('-----------------------')
+        else:
+            print('Error: Please type"python3 add2.py test1"')
+
+
+if __name__ == '__main__':
+    main()
